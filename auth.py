369d1bba5c801a1e5b4efd39c7389a7664d34e4d
DigitalXAOC <digitalxaoc@mail.ru>
2017-01-27 17:28:49 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,7 +3,6 @@ with open('userinfo') as uinfo:
     data=uinfo.readlines()
     data=[i.rstrip() for i in data]
     print(data)
-
 '''    E=poplib.POP3(data[0])
     E.user(data[1])
     E.pass_(data[2])
