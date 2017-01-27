39dd23e24215ee8f2a21c829f530d4814e6daf95
DigitalXAOC <digitalxaoc@mail.ru>
2017-01-27 17:19:02 +0300

    add auth.py & userinfo

---
+++ auth.py
@@ -0,0 +1,11 @@
+import poplib
+with open('userinfo') as uinfo:
+    data=uinfo.readlines()
+    data=[i.rstrip() for i in data]
+    print(data)
+'''    E=poplib.POP3(data[0])
+    E.user(data[1])
+    E.pass_(data[2])
+    print(E.retr(1)[1])
+    E.quit()
+'''
